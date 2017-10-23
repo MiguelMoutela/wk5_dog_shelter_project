990cfbbe0c10657dd27d74819ca8cea1ba44b6c3
Miguel <miguelmoutela@hotmail.com>
2017-10-23 12:19:55 +0100

    class patron - all properties defined; tests after lunch

--- models/patron.rb
+++ models/patron.rb
@@ -14,4 +14,67 @@ class Patron
 
   end
 
+  def save()
+    sql = "INSERT INTO patrons(
+    name,
+    adress,
+    email,
+    phone,
+    bio
+    )
+    VALUES(
+      $1, $2, $3, $4, $5
+    )
+    RETURNING .*"
+    values = [@name, @adress, @email, @phone, @bio]
+    result = SqlRunner.run( sql, values )
+    @id = result.first()['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE patrons
+           SET(
+             name,
+             adress,
+             email,
+             phone,
+             bio
+             ) =
+            ( $1, $2, $3, $4, $5
+            )
+            WHERE id = $6"
+    values = [@name, @adress, @email, @phone, @bio]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.delete( id )
+    sql = "DELETE * FROM patrons
+          WHERE id = $1"
+    values = [id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM patrons"
+    values = []
+    SqlRunner.run( sql, values )
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM patrons
+           WHERE id = $1"
+    values = [id]
+    result = Patron.new(SqlRunner.run( sql, values).first)
+    return result
+  end
+
+  def dogs
+      sql = "SELECT * FROM dogs WHERE id = $1"
+
+      values = [id]
+      dogs = SQLRunner.run(sql, values)
+      result = dogs.map {|dog| Dog.new(dog)}
+      return result
+  end
+
 end
